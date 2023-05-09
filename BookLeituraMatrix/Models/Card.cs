44515b379566b0f01f0716a4ede5bea48234a60e
Daniel <115712101+icaro1300@users.noreply.github.com>
2023-05-09 11:56:26 -0300

    .

---
+++ BookLeituraMatrix/Models/Card.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookLeituraMatrix.Models
+{
+    public class Card
+    {
+        public int CARID { get; set; }
+        public string CARTEXTO { get; set; }
+    }
+}

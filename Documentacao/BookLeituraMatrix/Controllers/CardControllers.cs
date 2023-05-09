8f6930e3bd293960219a11d12d45bb3be604f586
Daniel <115712101+icaro1300@users.noreply.github.com>
2023-05-09 11:58:37 -0300

    .

---
+++ Documentacao/BookLeituraMatrix/Controllers/CardControllers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookLeituraMatrix.Controllers
+{
+    public class CardControllers
+    {
+        public IActionResult Index()
+        {
+            CardTextoDAO dados = new ResponsavelDAO();
+            ViewBag.listaResponsavel = dados.getTodaOsResponsavel();
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
